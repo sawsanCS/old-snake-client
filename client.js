f368a9288be8372daadc90ab0e2af0ef202d4422
saoussen <sawsan_sli3i@live.fr>
2020-11-04 14:30:04 +0000

    modified the js file to add directions and move the snake

--- client.js
+++ client.js
@@ -2,26 +2,43 @@ const net = require('net');
 const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
-    port: 50541,
-    name: 'SSM'  });
+    port: 50541
+    });
     
   conn.on ('data', (data) => {
     console.log('Successfully connected to game server');
+    
   });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
   conn.on('connect', () => {
-    conn.write('Name: SSM');
-  });
+    let t = 0; 
 
+    conn.write('Name: SSM');
+    setTimeout(function() {
+      conn.write("Move: right");//print char
+    }, t+=500);
+    ;
+    setTimeout(function() {
+      conn.write("Move: up");//print char
+    }, t+=1000);
+    setTimeout(function() {
+      conn.write("Move: left");//print char
+    }, t+=1500);
+   // conn.write("Move: up");
+   // conn.write("Move: left");
+   // conn.write("Move: up");
+    
+  });
+  
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
   return conn;
 }
 
-connect();
+
 module.exports = {connect, connect};
 /*({
   host: host, 
