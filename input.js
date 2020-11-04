9768ec3f9c5aa6bb6c1403371b75048d4201843f
saoussen <sawsan_sli3i@live.fr>
2020-11-04 16:26:01 +0000

    added message with Say

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
+let connection;
 const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -8,13 +10,17 @@ const setupInput = function(conn) {
     if(key === '\u0003') {
       process.exit();
     }
-    if (key === 'w') {conn.write('Move: up')};
-    if (key === 's') {conn.write('Move: down')};
-    if (key === 'a') {conn.write('Move: left')};
-    if (key === 'd') {conn.write('Move: right')};
+    if (key === 'w') {connection.write('Move: up'); console.log('up');};
+    if (key === 's') {connection.write('Move: down'); console.log('down');};
+    if (key === 'a') {connection.write('Move: left'); console.log('left');};
+    if (key === 'd') {connection.write('Move: right'); console.log('right');};
+    if (key === 'q') {connection.write('Say: Respect each other'); };
+    /*if (key === 's') {conn.write('Move: down'); console.log('down');};
+    if (key === 'a') {conn.write('Move: left'); console.log('left');};
+    if (key === 'd') {conn.write('Move: right'); console.log('right');};*/
     
   }
   stdin.on('data', handleUserInput) //callback to pass any key strokes to hUI;
   return stdin;
 };
-module.exports = setupInput;
+module.exports = {setupInput, setupInput};
